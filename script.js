ed2ebf78cd67415b0ac48722d823759670ca07d6
PonomarevAlexx <136852344+PonomarevAlexx@users.noreply.github.com>
2023-06-17 18:16:58 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,49 +1,60 @@
 'use strict';
 
-let bestScore = document.querySelector('.highscore');
-const btnNewGame = document.querySelector('.again');
+let score = 20,
+    randomNumber = random(),
+    enteredNumber = document.querySelector('.number-input');
+const displayRandomNumber = document.querySelector('.question'),
+      bestScore = document.querySelector('.highscore'),
+      btnCheckInput = document.querySelector('.check'),
+      displayScore = document.querySelector('.score'),
+      body = document.querySelector('body'),
+      message = document.querySelector(('.guess-message')),
+      btnNewGame = document.querySelector('.again');
 
-    let score = 20,
-        randomNumber = Math.trunc((Math.random() * 20) + 1);
-    const displayRandomNumber = document.querySelector('.question'),
-          btnCheckInput = document.querySelector('.check'),
-          displayScore = document.querySelector('.score');
+function random() {
+    return Math.trunc((Math.random() * 20) + 1);
+};
 
-    btnCheckInput.addEventListener('click', () => {
-        let enteredNumber = document.querySelector('.number-input');
-        const message = document.querySelector(('.guess-message')),
-              body = document.querySelector('body');
+function noName(text) {
+    message.textContent = text;
+    score--;
+    displayScore.textContent = score;
+};
 
-        if (score < 2){
-            message.textContent = 'Game Over!';
-            displayScore.textContent = score - 1;
-        } else {
-            if(randomNumber == enteredNumber.value) {
-                message.textContent = 'Right!!!';
-                body.style.backgroundColor = 'green';
-                if (bestScore.textContent < score) {
-                    bestScore.textContent = score;
-                }
-                displayRandomNumber.textContent = randomNumber;
-            } else if (enteredNumber.value < randomNumber) {
-                message.textContent = 'Need more!';
-                score--;
-                displayScore.textContent = score;
-            } else if (enteredNumber.value > randomNumber) {
-                message.textContent = 'Need less!';
-                score--;
-                displayScore.textContent = score;
+btnCheckInput.addEventListener('click', () => {
+
+    if (score < 2){
+        message.textContent = 'Game Over!';
+        displayScore.textContent = score - 1;
+    } else {
+
+        if(randomNumber == enteredNumber.value) {
+            message.textContent = 'Right!!!';
+            body.style.backgroundColor = 'green';
+            displayRandomNumber.textContent = randomNumber;
+
+            if (bestScore.textContent < score) {
+                bestScore.textContent = score;
             }
         }
+        
+        if (enteredNumber.value < randomNumber) {
+            noName('Need more!');
+        }
+        
+        if (enteredNumber.value > randomNumber) {
+            noName('Need less!');
+        }
+    }
+});
 
-        btnNewGame.addEventListener('click', () => {
+btnNewGame.addEventListener('click', () => {
 
-            body.style.backgroundColor = 'black';
-            displayRandomNumber.textContent = '???';
-            message.textContent = 'Начни угадывать';
-            displayScore.textContent = '20';
-            enteredNumber.value = '';
-            randomNumber = Math.trunc((Math.random() * 20) + 1);
-            score = 20;
-        })
-    });
+    body.style.backgroundColor = 'black';
+    displayRandomNumber.textContent = '???';
+    message.textContent = 'Начни угадывать';
+    displayScore.textContent = '20';
+    enteredNumber.value = '';
+    randomNumber = random();
+    score = 20;
+});
