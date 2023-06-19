a1adc4cb6c9f54dc4232d0a55e12f9958b6cddee
PonomarevAlexx <136852344+PonomarevAlexx@users.noreply.github.com>
2023-06-19 16:12:59 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,49 +1,64 @@
 'use strict';
 
 let score = 20,
-    randomNumber = random(),
-    enteredNumber = document.querySelector('.number-input');
+    randomNumber = random();
 const displayRandomNumber = document.querySelector('.question'),
       bestScore = document.querySelector('.highscore'),
       btnCheckInput = document.querySelector('.check'),
       displayScore = document.querySelector('.score'),
       body = document.querySelector('body'),
       message = document.querySelector(('.guess-message')),
-      btnNewGame = document.querySelector('.again');
+      btnNewGame = document.querySelector('.again'),
+      enteredNumber = document.querySelector('.number-input');
+
+const texrMessage = {
+    startGame: 'Start playing',
+    needMore: 'Need more!',
+    needLess: 'Need less!',
+    win: 'Right!!!',
+    gameOver: 'Game Over!'
+}
 
 function random() {
     return Math.trunc((Math.random() * 20) + 1);
 };
 
-function noName(text) {
+function numberIsIncorrect(text) {
     message.textContent = text;
-    score--;
-    displayScore.textContent = score;
+    displayScore.textContent = --score;
 };
+
+function gameWasWin(text) {
+    message.textContent = text;
+    body.style.backgroundColor = 'green';
+    displayRandomNumber.textContent = randomNumber;
+    if (bestScore.textContent < score) {
+        bestScore.textContent = score;
+    }
+}
+
+function gameWasLost(text) {
+    message.textContent = text;
+    displayScore.textContent = score - 1;
+}
 
 btnCheckInput.addEventListener('click', () => {
 
     if (score < 2){
-        message.textContent = 'Game Over!';
-        displayScore.textContent = score - 1;
+        gameWasLost(texrMessage.gameOver)
     } else {
 
-        if(randomNumber == enteredNumber.value) {
-            message.textContent = 'Right!!!';
-            body.style.backgroundColor = 'green';
-            displayRandomNumber.textContent = randomNumber;
-
-            if (bestScore.textContent < score) {
-                bestScore.textContent = score;
-            }
+        if(randomNumber === +enteredNumber.value) {
+            gameWasWin(texrMessage.win)
+            return;
         }
         
         if (enteredNumber.value < randomNumber) {
-            noName('Need more!');
+            numberIsIncorrect(texrMessage.needMore);
         }
         
         if (enteredNumber.value > randomNumber) {
-            noName('Need less!');
+            numberIsIncorrect(texrMessage.needLess);
         }
     }
 });
@@ -52,7 +67,7 @@ btnNewGame.addEventListener('click', () => {
 
     body.style.backgroundColor = 'black';
     displayRandomNumber.textContent = '???';
-    message.textContent = 'Начни угадывать';
+    message.textContent = texrMessage.startGame;
     displayScore.textContent = '20';
     enteredNumber.value = '';
     randomNumber = random();
